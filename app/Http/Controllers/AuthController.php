66b2d2c67f2b1a071dcbb6ee78ff23b8b3372231
Gintautas Miselis <gintautas.miselis@eckoh.com>
2016-09-01 17:47:10 +0100

    Authenticate and store token in database

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AuthController extends Controller
+{
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function login(Request $request)
+    {
+        $user = $request->user();
+        if ($user === null) {
+            return response()->json(['error' => 'incorrect username or password'], 401);
+        }
+
+        $token = 'test';
+
+        DB::insert('insert into token (token, ip, username) values (?, ?, ?)', [
+            $token, $request->getClientIp(), $user->username
+        ]);
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function logout(Request $request)
+    {
+
+    }
+
+    /**
+     * @param $username
+     * @param $password
+     * @return bool
+     */
+    private function isValidUser($username, $password)
+    {
+        return defined('RUNNING_TESTS') && $username === 'valid';
+    }
+}
