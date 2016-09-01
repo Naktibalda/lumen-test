97d72e758cd913ad965343364de6db4117edc84c
Gintautas Miselis <gintautas.miselis@eckoh.com>
2016-09-01 18:58:46 +0100

    Added db dump

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,1 +1,7 @@
-/* Replace this file with actual dump of your database */
+CREATE TABLE `token` (
+  `token` char(36) NOT NULL,
+  `ip` int(10) unsigned NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
